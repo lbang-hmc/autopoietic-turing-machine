e62e31434ca29dc65e73a2b02f5a441e9e8316d2
Lucas Bang <bang@cs.hmc.edu>
2020-12-17 16:34:00 -0800

    colors, cursor, etc.

--- py/main.py
+++ py/main.py
@@ -3,7 +3,6 @@ from TuringMachine import *
 import time
 from relay import Relay
 
-
 class FakeRelay:
     def __init__(self):
         self.relaystate = ""
@@ -14,7 +13,7 @@ class FakeRelay:
 # relay = Relay(idVendor=0x16c0, idProduct=0x05df)
 relay = FakeRelay()
 
-def move_by_keypress(tm, key):
+def type_or_move_by_keypress(tm, key):
     if (key == 'h'):
         tm.move('up')
     elif (key == 'j'):
@@ -23,39 +22,58 @@ def move_by_keypress(tm, key):
         tm.move('left')
     elif(key == 'l'):
         tm.move('right')
-    else:
-        print('unknown direction, ' + key)
-
-def type_by_keypress(tm, key):
-    if(key == 'd'):
+    elif(key == 'd'):
         tm.write(0)
     elif(key == 'f'):
         tm.write(1)
     else:
-        print('unknown tape symbol, ' + key)
+        print('unknown key: ' + key)
 
+
+# need function for this graphics initialization
 win = GraphWin(width = 1800, height = 950) # create a window
 win.setCoords(0, 0, 100, 100) # set the coordinates of the window; bottom left is (0, 0) and top right is (100, 100)
+win_color = color_rgb(20,20,20)
+win.setBackground(win_color)
 x = 50
 y = 50
 win.master.geometry('%dx%d+%d+%d' % (1800, 950, x, y))
 mySquare = Rectangle(Point(1, 1), Point(99, 99)) # create a rectangle from (1, 1) to (99, 99)
+mySquare.setOutline("white")
 mySquare.draw(win) # draw it to the window
 # win.getMouse() # pause before closing
 
+
+text_color = color_rgb(240,240,220)
+
+
+cursor_x = 4.6
+cursor_y = 87.8
+cursor_h = 2
+cursor_w = 0.75
+cursor_box = Rectangle(Point(cursor_x, cursor_y), Point(cursor_x + cursor_w, cursor_y + cursor_h))
+cursor_box.setOutline("white")
+cursor_box.draw(win)
+
 tm = TuringMachine(5)
 
-fontsize = 14
+fontsize = 15
 
+
+# need a function to do all this text initialization
 bit_text = Text(Point(10,50), str(tm))
 bit_text.setFace("courier")
-bit_text.setSize(fontsize)    
-bit_text.draw(win)
+bit_text.setSize(fontsize)
+bit_text.setTextColor(text_color)    
+# bit_text.draw(win)
 
-cursor_chars = list(str(tm).replace('0', ' ').replace('1', ' '))
+cursor_chars = list(str(tm))
+# cursor_chars = list(str(tm).replace('0', ' ').replace('1', ' '))
 cursor_chars[(tm.width+1) * tm.y + tm.x] = 'X'
 cursor_str = "".join(cursor_chars)
 cursor_text = Text(Point(10,50), cursor_str)
+cursor_text.setStyle("bold")
+cursor_text.setTextColor(text_color)
 
 cursor_text.setFace("courier")
 cursor_text.setSize(fontsize)    
@@ -64,13 +82,20 @@ cursor_text.draw(win)
 
 instr_text = Text(Point(55.0,51.1), tm.human_readable())
 instr_text.setFace("courier")
-instr_text.setSize(fontsize)    
+instr_text.setSize(fontsize)
+instr_text.setTextColor(text_color)
+instr_text.setStyle("bold")    
 instr_text.draw(win)
 
 todo_text = Text(Point(30, 95), '')
 todo_text.setFace("courier")
 todo_text.setSize(fontsize)
+todo_text.setTextColor(text_color)
 todo_text.draw(win)
+
+
+
+
 
 def handle_typing(to_type):
     if(to_type):
@@ -96,26 +121,24 @@ i = 0
 done = False
 while (i < 10 and not done):
     i = i + 1
-    # tm.step()
     todo = tm.what_to_do()
     todo_text.setText(str(todo) + ' [' + str(tm.x) + ', ' + str(tm.y) + ']')
 
 
-    # implementation is expecting a write (0/1) then move (u/d/l/r)
-    # should make it just wait for arbitrary key press then just 
-    # execute that keypress.
-
-
-    # type_key = win.getKey()
+    # tell the hand (relay) to push the right button to type a 0 or 1
     type_key = handle_typing(todo['type'])
     if(type_key == 'q'):
         done = True
-    type_by_keypress(tm, type_key)
 
 
+
+    type_or_move_by_keypress(tm, type_key)
+
+    # need a text update function that does all this
     bit_text.setText(str(tm))
     instr_text.setText(tm.human_readable())
-    cursor_chars = list(str(tm).replace('0', ' ').replace('1', ' '))
+    # cursor_chars = list(str(tm).replace('0', ' ').replace('1', ' '))
+    cursor_chars = list(str(tm))
     cursor_chars[(tm.width+1) * tm.y + tm.x] = 'X'
     cursor_str = "".join(cursor_chars)
     cursor_text.setText(cursor_str)
@@ -124,22 +147,53 @@ while (i < 10 and not done):
 
     time.sleep(0.5)
 
-
-    # move_key = win.getKey()
     move_key = handle_moving(todo['move'].strip())
-    if(type_key == 'q'):
+    if(move_key == 'q'):
         done = True
-    move_by_keypress(tm, move_key)
+   
+   
+    type_or_move_by_keypress(tm, move_key)
     
+    # need a text update function that does all this
+    bit_text.setText(str(tm))
+    instr_text.setText(tm.human_readable())
+    # cursor_chars = list(str(tm).replace('0', ' ').replace('1', ' '))
+    cursor_chars = list(str(tm))
+    cursor_chars[(tm.width+1) * tm.y + tm.x] = 'X'
+    cursor_str = "".join(cursor_chars)
+    cursor_text.setText(cursor_str)
     
     tm.state = todo['next']
-    time.sleep(0.1)
     time.sleep(0.5)
-# for i in range(5):
-#     key = win.getKey()
-#     print(key)
-#     t.setText("x: \n" + key)
-#     # t.draw(win)
+
+
     
 win.getKey()
 win.close()
+
+
+# def move_by_keypress(tm, key):
+#     if (key == 'h'):
+#         tm.move('up')
+#     elif (key == 'j'):
+#         tm.move('down')
+#     elif (key == 'k'):
+#         tm.move('left')
+#     elif(key == 'l'):
+#         tm.move('right')
+#     else:
+#         print('unknown direction, ' + key)
+
+# def type_by_keypress(tm, key):
+#     if(key == 'd'):
+#         tm.write(0)
+#     elif(key == 'f'):
+#         tm.write(1)
+#     else:
+#         print('unknown tape symbol, ' + key)
+
+
+
+# implementation is expecting a write (0/1) then move (u/d/l/r)
+# should make it just wait for arbitrary key press then just 
+# execute that keypress.
